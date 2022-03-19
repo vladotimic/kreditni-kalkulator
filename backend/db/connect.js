772fcd84318fa013118667a6e1ea3414cfa5f712
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2022-03-19 15:02:41 +0100

    created .env file

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-// Znam da nije ovakva praksa konekcije na bazu, ovo je samo za ove svrhe
-// Inace stavljam u .env file
-const connectToDB = mongoose.connect(
-  'mongodb+srv://vlado:1234@cluster0.bx8if.mongodb.net/loans?retryWrites=true&w=majority'
-);
+const connectToDB = mongoose.connect(process.env.MONGO_URI);
 
 module.exports = connectToDB;
